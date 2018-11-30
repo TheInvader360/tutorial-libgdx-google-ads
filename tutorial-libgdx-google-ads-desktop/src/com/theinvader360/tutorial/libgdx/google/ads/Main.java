f695cd03db06b75b3c3aa21ac995394fb5a38545
SimonIT <simonit.orig@gmail.com>
2018-11-30 23:03:17 +0100

    reformat

--- tutorial-libgdx-google-ads-desktop/src/com/theinvader360/tutorial/libgdx/google/ads/Main.java
+++ tutorial-libgdx-google-ads-desktop/src/com/theinvader360/tutorial/libgdx/google/ads/Main.java
@@ -4,11 +4,11 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
 public class Main {
-	public static void main(String[] args) {
-		Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
-		cfg.setTitle("tutorial-libgdx-google-ads");
-		cfg.setWindowedMode(480,320);
-		
-		new Lwjgl3Application(new AdTutorial(new ActionResolverDesktop()), cfg);
-	}
+    public static void main(String[] args) {
+        Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
+        cfg.setTitle("tutorial-libgdx-google-ads");
+        cfg.setWindowedMode(480, 320);
+
+        new Lwjgl3Application(new AdTutorial(new ActionResolverDesktop()), cfg);
+    }
 }
